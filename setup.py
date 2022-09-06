6ebb0e5cd905d758c2dfcf2a338c0fe69cc71a72
Tom Bennett <tombennett86@hotmail.co.uk>
2022-09-06 15:44:05 +0100

    add setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,11 @@
-"""
-Setuptools setup module
-"""
 from codecs import open
 from os import path
-from re import compile as re_compile
 
 import toml
 from setuptools import find_packages, setup
 
-"""
-NOTE: As of PEP-518 (https://www.python.org/dev/peps/pep-0518/)
-and PEP-621 (https://www.python.org/dev/peps/pep-0621/)
-setup.py is no longer the preferred way to build
-and package python libraries. However, to avoid the duplication of information
-in both pyproject.toml and setup.cfg, until setuptools reading from
-pyproject.toml is supported (see: https://github.com/pypa/setuptools/issues/1688),
-the information is pulled from pyproject.toml to use in here.
-"""
-
 HERE = path.abspath(path.dirname(__file__))
 
-# Get the requirements from the requirements.txt
 with open(path.join(HERE, 'pyproject.toml'), encoding='utf-8') as f:
     project_meta = toml.load(f)['project']
 
