797a2f89a175998bb74fdae66c656809408665d3
Tom Bennett <tombennett86@hotmail.co.uk>
2022-09-06 15:56:54 +0100

    add setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,4 @@
-from codecs import open
-from os import path
+from setuptools import setup
 
-import toml
-from setuptools import find_packages, setup
-
-HERE = path.abspath(path.dirname(__file__))
-
-with open(path.join(HERE, 'pyproject.toml'), encoding='utf-8') as f:
-    project_meta = toml.load(f)['project']
-
-setup(
-    name=project_meta['name'],
-    description=project_meta['description'],
-    author=[item['name'] for item in project_meta['authors'] if 'name' in item][0],
-    author_email=[item['email'] for item in project_meta['authors'] if 'email' in item][
-        0
-    ],
-    packages=find_packages(exclude=['*.tests', '*.tests.*', 'tests.*', 'tests']),
-    install_requires=project_meta['dependencies'],
-    extras_require=project_meta['optional-dependencies'],
-    python_requires=project_meta['requires-python'],
-    classifiers=project_meta['classifiers'],
-)
+if __name__ == "__main__":
+    setup()
